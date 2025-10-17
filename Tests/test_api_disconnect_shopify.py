332c666479cd8f2ccc8f411bdf7d35687c364cd1
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-17 16:44:44 +0200

    Change feature tag to story in test file

--- Tests/test_api_disconnect_shopify.py
+++ Tests/test_api_disconnect_shopify.py
@@ -6,7 +6,7 @@ TITLE = "Disconnect API"
 @allure.tag("Automation")
 @allure.epic("Test Cases")
 @allure.suite("Master")
-@allure.feature("Disconnect API")            
+@allure.story("Disconnect API")            
 @allure.title(TITLE)
 @allure.label("owner", "Shivam Sanjay Desale")
 @allure.label("priority", "Medium")
