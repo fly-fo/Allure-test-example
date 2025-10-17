dc0f22b1bbb708e8d5bc98b7230f49f5cecb48e5
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-17 16:47:20 +0200

    Update and rename test_API_sync_order.py to test_api_sync_order.py

--- Tests/test_api_sync_order.py
+++ Tests/test_api_sync_order.py
@@ -6,7 +6,7 @@ TITLE = "Sync Order API"
 @allure.tag("Automation")
 @allure.epic("Test Cases")
 @allure.suite("Master")
-@allure.feature("Sync Order API")
+@allure.story("Sync Order API")
 @allure.title(TITLE)
 @allure.label("owner", "Shivam Sanjay Desale")
 @allure.label("priority", "Medium")
