7ea39df5846ee42c30d715e6d2e328fd9539e973
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-20 18:11:41 +0200

    Change priority label to P0 and add product label
    Updated priority label and added product label in test.

--- Tests/test_api_disconnect_shopify.py
+++ Tests/test_api_disconnect_shopify.py
@@ -9,7 +9,18 @@ TITLE = "Disconnect API"
 @allure.story("Disconnect API")            
 @allure.title(TITLE)
 @allure.label("owner", "Shivam Sanjay Desale")
-@allure.label("priority", "Medium")
+@allure.label("priority", "P0")
+@allure.label("product", "pitneyship")
+Custom fields
+
+Active 13
+Deleted
+Search by name
+
+Updated
+
+What are custom fields?
+With custom fields you can mark up and filter data, organize test cases and more
 @allure.label("trProject", "Connectors")
 @allure.label("layer", "API")
 @pytest.mark.parametrize(
