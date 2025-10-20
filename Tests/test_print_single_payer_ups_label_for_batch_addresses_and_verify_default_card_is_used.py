449d5a2b70d911d1c59f9e9fe06458b753d0fec9
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-10-20 18:22:11 +0200

    Add product labels for UPS label test

--- Tests/test_print_single_payer_ups_label_for_batch_addresses_and_verify_default_card_is_used.py
+++ Tests/test_print_single_payer_ups_label_for_batch_addresses_and_verify_default_card_is_used.py
@@ -10,6 +10,8 @@ TITLE = "Print Single Payer UPS label for batch addresses and verify default car
 @allure.title(TITLE)
 @allure.label("owner", "Sowmya Katamaneni")
 @allure.label("priority", "Medium")
+@allure.label("product", "pitneyship")
+@allure.label("product", "shipaccel")
 @allure.label("trProject", "Shipaccel")
 @allure.label("layer", "UI")
 @pytest.mark.parametrize("address", ["CT", "NY"], ids=["CT", "NY"])
