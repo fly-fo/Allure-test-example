a57128f017d557717e5c7c15bfb94853c1b844de
Amir Suleyman <118766479+fly-fo@users.noreply.github.com>
2025-12-24 17:19:34 +0100

    Update Allure labels and tags in test file

--- Tests/test_verify_manifest_pickup_not_available_single_payer_FEDEX_CC_address.py
+++ Tests/test_verify_manifest_pickup_not_available_single_payer_FEDEX_CC_address.py
@@ -4,15 +4,16 @@ import allure
 TITLE = "Verify manifest and pickup not available for single payer FEDEX for CC card with address"
 
 @allure.tag("Automation")
-@allure.epic("UI Automation")
-@allure.suite("ShipAccel UI Automation")
+@allure.epic("Automation")
+@allure.suite("Allure Automation")
 @allure.story(TITLE)
 @allure.title(TITLE)
-@allure.label("owner", "Sowmya Katamaneni")
+@allure.layer("Unit Tests")
+@allure.label("owner", "Amir")
 @allure.label("priority", "Medium")
-@allure.label("product", "shipaccel")
-@allure.label("trProject", "Shipaccel")
-@allure.label("layer", "UI")
+@allure.label("product", "Allure")
+@allure.label("trProject", "Allure testops")
+@allure.label("layer", "Unit Tests")
 @pytest.mark.parametrize("address", ["CT", "NY"], ids=["CT", "NY"])
 def test_manifest_pickup_not_available_fedex_cc(address):
     with allure.step(f"Open shipping context for address={address} (Single Payer FEDEX, CC card set)"):
